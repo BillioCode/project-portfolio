ef991b8c036f5e73a8c96a3757cf9fd944441b96
Billel Rachedi <snoprwaystv@gmail.com>
2024-10-11 17:52:28 +0100

    bug fixes

--- app.js
+++ app.js
@@ -1,36 +1,27 @@
 const express = require("express");
 const app = express(); 
-const exphbs = require("express-handlebars");
+const exphbs = require("express-handlebars").create; 
 const path = require("path");
-
-
 
 const port = process.env.PORT || 8080;
 
 
-
-
 app.set("views", path.join(__dirname, "views"));
+
 
 app.engine(".hbs", exphbs({
     extname: ".hbs",
     defaultLayout: false
-}));
+}).engine); 
 
 app.set("view engine", ".hbs");
 
-
+// Route to render the home view
 app.get('/', (req, res, next) => {
-    res.render("home.hbs");
-})
-
-
-
-
+    res.render("home"); 
+});
 
 
 app.listen(port, () => {
     console.log("Server is up on port:", port);
-})
-
-
+});

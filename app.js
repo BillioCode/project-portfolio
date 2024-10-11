e64dc5b26c368caf72d813918781754ce145712c
Billel Rachedi <snoprwaystv@gmail.com>
2024-10-11 17:12:42 +0100

    added node js path and handlebars ( first time i work with this )

--- app.js
+++ app.js
@@ -1,7 +1,26 @@
-let express = require("express");
+const express = require("express");
 const app = express(); 
+const exphbs = require("express-handlebars");
+const path = require("path");
+
+
 
 const port = process.env.PORT || 8080;
+
+
+
+
+app.set("views", path.join(__dirname, "views"));
+app.engine(".hbs", exphbs({
+    extname: ".hbs",
+    defaultLayout: false
+}));
+
+app.set("view engine", ".hbs");
+
+
+
+
 
 app.get('/', (req, res) => {
     res.send('index Routes is working');
